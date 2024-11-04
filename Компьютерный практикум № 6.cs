542a2fc2d8fc78ba3bb05a8471cd3ce4058fd788
sap1ks <sap1kcsgo@gmail.com>
2024-11-04 20:00:48 +0200

    Add files via upload

---
+++ Компьютерный практикум № 6.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("a= ");
+            //double a=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b=Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"{a}+{b}={a+b}");
+            //Console.ReadKey();
+
+            //Console.Write("a=");
+            //double a=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"{a}+{b}={b}+{a}");
+            //Console.ReadKey();
+
+            //Console.Write("a=");
+            //double a=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"{a:f1}*{b:f1}={a * b:f1}");
+            //Console.ReadLine();
+
+            //Console.Write("a=");
+            //double a=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b=Convert.ToDouble(Console.ReadLine());
+            //Console.Write("c=");
+            //double c=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"({a:f2}+{b:f2})+{c:f2}={a:f2}+({b:f2}+{c:f2})");
+            //Console.ReadLine();
+
+            //    Console.Write("a=");
+            //    double a = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("b=");
+            //    double b = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("c=");
+            //    double c = Convert.ToDouble(Console.ReadLine());
+            //    Console.Write("d=");
+            //    double d = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine($"{a:f2}/{b:f2}+{c:f2}/{d:f2}={a / b + c / d:f2}");
+            //    Console.ReadLine();
+
+            //Console.Write("a=");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b=");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"a+b={a}+{b}={a + b}\t");Console.Write($"a-b={a}-{b}={a - b}\t");Console.Write($"a*b={a}*{b}={a*b}");
+            //Console.ReadKey();
+
+            //Console.Write("a= ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("с=");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"[a*(b+c)]=[(b+c)*a]:\t[{a}*({b}+{c})]=[({b}+{c})*{a}]={a * (b + c)}");
+            //Console.ReadLine();
+
+            //Console.Write("a= ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("с= ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"({a:f4}+({b:f4}+{c:f4})=({a:f4}+{c:f4}+{b:f4})");
+            //Console.ReadKey();
+
+            //Console.Write("x= ");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("y= ");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"округленный с точностью до 4 знаков результат:\t{x*3:f4}/{y}= {x*3/y:f4}");
+            //Console.ReadLine();
+
+            //Console.Write("a= ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("b= ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.Write($"a*b={a:f3}*{b:f3}={a * b:f3}\t"); Console.Write($"a*b={a:f5}*{b:f5}={a * b:f5}");
+            //Console.ReadLine();
+        }
+    }
+}
